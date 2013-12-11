e29a037bce1538f02c462b84a232bf8b9a7b8735
TzahiM <tzahimanmobile@gmail.com>
2013-12-11 19:13:54 +0200

    adding pages list

--- src/pages/views.py
+++ src/pages/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+#from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+from pages.models import Page
+
+def list_of_all_pages(request):
+    text_string = "<ol>\n"
+    for page in Page.objects.all():
+        text_string += "<li>" + page.name + "</li>\n" 
+    text_string += "</ol>\n"
+    return HttpResponse( text_string) 
+    
+    
--- src/wordwiki/urls.py
+++ src/wordwiki/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'wordwiki.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^word/', include('pages.urls', namespace="pages")),
     url(r'^admin/', include(admin.site.urls)),
 )
