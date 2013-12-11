565589cfa9acb2e40ffe75328c33f2150335ba00
TzahiM <tzahimanmobile@gmail.com>
2013-12-11 18:26:12 +0200

    adding admin

--- src/pages/admin.py
+++ src/pages/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from pages.models import Page
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('name','header', 'create_date','update_date')
+    ordering = ['name']
+    search_fields = ['header', 'body_text']
+    
+
+admin.site.register(Page, PageAdmin)
--- src/pages/tests.py
+++ src/pages/tests.py
@@ -6,5 +6,5 @@ class PageTests(TestCase):
     def test_page_creation(self):
         page1st = Page(name = '1st', header = 'First header')
         page2nd = Page(name = '2nd', header = 'Second header')
-        self.assertEqual(page1st.name, '1st')
+        self.assertEqual(page1st.name, '1st' )
         self.assertEqual(page2nd.name, '2nd')
