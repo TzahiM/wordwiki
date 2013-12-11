7b7049f288a5b772c82b29b8ffec5a37efa0c5aa
TzahiM <tzahimanmobile@gmail.com>
2013-12-11 15:05:53 +0200

    page model tested

---
+++ src/pages/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.core.validators import MaxLengthValidator 
+
+# Create your models here.
+
+class Page(models.Model):
+    name = models.CharField(primary_key = True, max_length=200, validators=[MaxLengthValidator])
+    header = models.CharField(blank=True, max_length=200)
+    create_date = models.DateTimeField('date created', auto_now_add=True )
+    update_date = models.DateTimeField('last-modifie', auto_now =True )
+
+    body_text = models.TextField(blank=True)
+    def __unicode__(self):  # Python 3: def __str__(self):
+        return self.name
+
---
+++ src/pages/tests.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+from pages.models import Page
+
+# Create your tests here.
+class PageTests(TestCase):
+    def test_page_creation(self):
+        page1st = Page(name = '1st', header = 'First header')
+        page2nd = Page(name = '2nd', header = 'Second header')
+        self.assertEqual(page1st.name, '1st')
+        self.assertEqual(page2nd.name, '2nd')
